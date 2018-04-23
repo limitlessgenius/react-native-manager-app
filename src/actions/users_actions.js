0b94b478825d013b48eb369bcf2f50198d52f921
José Manuel <expressmillionaire@hotmail.com>
2018-04-23 22:06:50 +0200

    users fetch correctly

--- src/actions/users_actions.js
+++ src/actions/users_actions.js
@@ -1,20 +1,17 @@
 
 
-
 import firebase from 'firebase'
 
-
-
 export const fetchUsers = () => {
-	// https://manager-dev-c287d.firebaseio.com/experiences.json
+	// https://manager-dev-c287d.firebaseio.com/users.json
 	// FIREBASE DATABASE END POINT
 	return(dispatch) => {
-		firebase.database().ref(`/users/`)
+		firebase.database().ref('/users/')
 			.on('value', snapshot => {
 				dispatch({
 					type: 'FETCH_USERS_SUCCESS', 
 					payload: snapshot.val()
-				})	
-		})	
+				})
+			})
 	}
 }
--- src/reducers/users_reducer.js
+++ src/reducers/users_reducer.js
@@ -8,7 +8,7 @@ export default (state = null, action) => {
 	switch(action.type){
 
 		case('FETCH_USERS_SUCCESS'): 
-			console.log(action)
+			console.log('USERS LIST', action.payload)
 			return state
 		default: 
 			return state
