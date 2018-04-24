75b19c63df091be842977a978505f1900660850f
José Manuel <expressmillionaire@hotmail.com>
2018-04-24 22:05:44 +0200

    server issue solved

--- src/components/component_chat_room.js
+++ src/components/component_chat_room.js
@@ -1,28 +1,33 @@
 
 
 import firebase from 'firebase'
-
-console.log(firebase)
-
-
-
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
-import {send, subscribe} from 'react-native-training-chat-server';
-//CHAT
+import { send, subscribe } from 'react-native-training-chat-server';
+//Change Firebase APP Name To Run Them Jointly
 
-
+const NAME = 'José Manuel';
+const CHANNEL = 'Reactivate';
 
 class ChatRoom extends Component {
 
+	state = {
+		messages: [],
+	};
 
-  render() {
-    return (
-      <View>CHAT ROOM</View>
-    );
+	componentDidMount() {
+		subscribe(CHANNEL, messages => {
+
+		this.setState({messages});
+    });
   }
 
+  render() {
+	return (
+		<View>CHAT ROOM</View>
+    );
+  }
 }
 export default ChatRoom
 
